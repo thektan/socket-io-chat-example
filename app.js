183bd9efaee3e80170c193549821b6025edecf39
Kevin Tan <kevin.tan@liferay.com>
2016-06-22 16:41:39 -0700

    Add disconnect event

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ io.on(
 	'connection',
 	function(socket) {
 		console.log('a user connected');
+
+		socket.on(
+			'disconnect',
+			function() {
+				console.log('user disconnected');
+			}
+		);
 	}
 );
 
